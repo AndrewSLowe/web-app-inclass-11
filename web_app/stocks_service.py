6260ab4b933e0b9f80b7e23dd218c732baeac8a8
AndrewSLowe <andrewstevenlowe@gmail.com>
2020-03-17 22:19:10 -0500

    added some notes to stocks_service.py

--- web_app/stocks_service.py
+++ web_app/stocks_service.py
@@ -4,7 +4,8 @@
 import requests
 import json
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=abc123"
+# With urls, look for api parameters to change what data I get. Also, this api uses authentification by url `apikey=abc123`
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=abc123" 
 print(request_url)
 
 response = requests.get(request_url)
@@ -12,7 +13,7 @@ print(type(response)) #> <class 'requests.models.Response'>
 print(response.status_code) #> 200
 print(type(response.text)) #> <class 'str'>
 
-data = json.loads(response.text)
+data = json.loads(response.text)   # Turns the response which is a json like string to a puthon dictionary
 print(type(data)) #> <class 'dict'>
 
 latest_close = data["Time Series (Daily)"]["2020-02-25"]["4. close"]
