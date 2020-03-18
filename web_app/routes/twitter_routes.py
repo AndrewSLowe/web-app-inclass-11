72f5526f6c31927ca1f88d6a797a38792fa2ea7c
AndrewSLowe <andrewstevenlowe@gmail.com>
2020-03-18 18:02:42 -0500

    fixed pipfiles

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -3,11 +3,10 @@ from flask import Blueprint, render_template
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
-@home_routes.route("/users/<screen_name>")
+@twitter_routes.route("/users/<screen_name>")
 def get_user(screen_name=None):
     print(screen_name)
 
     # user_info = {"screen_name": "__", "followers_count": 100}
-    user_info = api.get_user(screen_name)
 
-    return jsonify(user_info)
+    return render_template("user.htm", screen_name=screen_name)
